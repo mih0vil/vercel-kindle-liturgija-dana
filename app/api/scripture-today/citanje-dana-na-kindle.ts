5df46e2f92a262ba6e5602c42864968203ad3f04
Mihovil Frater <mihovil.frater@neos.hr>
2024-02-06 18:23:19 +0100

    uredi stranicu preko gumba

--- app/api/scripture-today/citanje-dana-na-kindle.ts
+++ app/api/scripture-today/citanje-dana-na-kindle.ts
@@ -1,7 +1,9 @@
+"use server"
 import fetchHtml from "@/app/api/scripture-today/fetch-html";
 import {sendEmail} from "@/app/api/scripture-today/send-email";
 
 export default async function citanjeDanaKindle() {
     const {html} = await fetchHtml();
     const email = await sendEmail(html ?? 'nisam uspio dohvati sadrzaj');
+    await email.json();
 }
