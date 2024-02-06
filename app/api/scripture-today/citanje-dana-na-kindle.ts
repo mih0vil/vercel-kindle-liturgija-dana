a044a1c1949fa16e4a7f7a0b60d6d93d889835b7
Mihovil Frater <mihovil.frater@neos.hr>
2024-02-06 20:51:15 +0100

    ucitavanje podataka

--- app/api/scripture-today/citanje-dana-na-kindle.ts
+++ app/api/scripture-today/citanje-dana-na-kindle.ts
@@ -6,6 +6,6 @@ export default async function citanjeDanaKindle() {
     const {html, date} = await fetchHtml();
     const email = await sendEmail(html ?? 'nisam uspio dohvati sadrzaj', date);
     // console.log({email});
-    // return `Email sent ${new Date().toISOString()}`;
+    // return `Liturgija dana ${date} \n ${html}`;
     return `Liturgija dana ${date} poslana ${email.SubmittedAt}  \n ${html}`;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 'use client'
 
 import citanjeDanaKindle from "@/app/api/scripture-today/citanje-dana-na-kindle";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 export default function Home() {
     const [data, formAction] = useFormState(citanjeDanaKindle, '');
-    // console.log({data})
+    // console.log({data});
+
     return (
         <form action={formAction} className={`w-full flex ${data ? '' : 'h-screen'} flex-col items-center justify-center`}>
             <button type={"submit"} className={"btn-primary"}>Pošalji čitanje dana na Kindle</button>
-            <div dangerouslySetInnerHTML={{__html: data}}/>
+            <Podatci data={data}/>
         </form>
     );
+}
+
+function Podatci({data}: { data: string }) {
+    const {pending} = useFormStatus();
+
+    return (pending ?
+            <p>Učitavam podatke sa stranice...</p>
+            : <div dangerouslySetInnerHTML={{__html: data}}/>
+    )
 }
