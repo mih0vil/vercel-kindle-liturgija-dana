6e2e717712a773cb5c61ff1a1d46becf49f456c0
Mihovil Frater <mihovil.frater@neos.hr>
2024-02-06 15:49:29 +0100

    pojednostavi html naziv

--- app/api/scripture-today/send-email.ts
+++ app/api/scripture-today/send-email.ts
@@ -8,13 +8,14 @@ export async function sendEmail(html: string) {
         const content = buffer.toString('base64');
         const to = process.env.KINDLE_EMAIL_ADDRESS ?? 'postavi email adresu';
         // const naslov = `Liturgija dana ${new Date().toISOString()}`;
-        const naslov = `Liturgija dana XXX`;
+        const naslov = `Liturgija-dana`;
         const {data, error} = await resend.emails.send({
-            from: '"Liturgija dana" <onboarding@resend.dev>',
+            // from: '"Liturgija dana" <onboarding@resend.dev>',
+            from: 'onboarding@resend.dev',
             // to: 'delivered@resend.dev',
             to: to,
             subject: naslov,
-            text: 'Automatska poruka',
+            text: 'Automatski generirana poruka',
             attachments: [
                 {content: content, filename: `${naslov}.html`,},
                 // {content: content, filename: `${naslov}.txt`,},
