bc2c1351f550dbdd5ce68e7e7a07153d2acf77b3
Mihovil Frater <mihovil.frater@neos.hr>
2024-02-12 11:21:13 +0100

    zadnji koristeni email

--- app/api/scripture-today/citanje-dana-na-kindle.ts
+++ app/api/scripture-today/citanje-dana-na-kindle.ts
@@ -19,6 +19,7 @@ export async function dohvatiPosaljiForm(previousState: Awaited<DohvatiPosaljiRe
 export type DohvatiPosaljiResp = {
     html?: string
     naslov?: string
+    email?: string
     emailSentAt?: string
     error?: string
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 "use client"
 import {Form} from "@react-spectrum/form";
-import {Button, DateRangePicker, Flex, Heading, ProgressCircle, TextField, View} from "@adobe/react-spectrum";
+import {
+    Button,
+    DateRangePicker,
+    Flex,
+    Heading,
+    ProgressCircle,
+    TextField,
+    View
+} from "@adobe/react-spectrum";
 import {dohvatiPosaljiForm, DohvatiPosaljiResp} from "@/app/api/scripture-today/citanje-dana-na-kindle";
 import {getLocalTimeZone, today} from "@internationalized/date";
 import {useFormState} from "react-dom";
 import {isDev} from "@/env-vars";
-import {useEffect, useState} from "react";
+import {FormEvent, useEffect, useState} from "react";
 
 const LoadingCircle = () => (
     <View><ProgressCircle aria-label="Loading…" isIndeterminate marginEnd={"size-250"} />Obrađujem podatke...</View>
@@ -19,6 +27,26 @@ export default function Home() {
 
     const now = today(getLocalTimeZone());
     const emailPattern = isDev ? undefined : ".+@kindle\\.com";
+    const emailAddressKey = "emailAddress"
+    const initialEmail = "@kindle.com";
+
+    const lastEmail = () => {
+        if (typeof window !== "undefined") {
+            const email = localStorage.getItem(emailAddressKey);
+            return email ?? initialEmail;
+        } else {
+            return initialEmail;
+        }
+    }
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        setPending(true);
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string | null
+        if (email) {
+            localStorage.setItem(emailAddressKey, email)
+        }
+    }
 
     useEffect(() => {
         setPending(false)
@@ -27,7 +55,7 @@ export default function Home() {
 
     return (
         <Flex direction={"row"} justifyContent={"center"} minHeight={"100vh"} alignItems={"center"}>
-            <Form maxWidth="size-6000" validationBehavior="native" action={formAction} onSubmit={() => setPending(true)}>
+            <Form maxWidth="size-6000" validationBehavior="native" action={formAction} onSubmit={onSubmit}>
                 <Heading level={1}>
                     Pošalji čitanja dana za određeni vremenski period
                 </Heading>
@@ -47,7 +75,7 @@ export default function Home() {
                 />
 
                 <TextField name={"email"} type={"email"} label="Email adresa tvog Kindle-a"
-                           defaultValue={"@kindle.com"}
+                           defaultValue={lastEmail()}
                            pattern={emailPattern}
                            description={"Ako želiš samo dohvatiti podatke bez slanja na Kindle, ostavi prazno"}
                 />
